f3b3bdacc2b613ed885c137f5aa48b537631e87b
Jeremy Falling <jjfalling@gmail.com>
2013-03-03 10:57:44 -0800

    Update super-pi-power.pl
    added license

--- super-pi-power.pl
+++ super-pi-power.pl
@@ -2,9 +2,25 @@
 use strict;
 use warnings;
 
-#############################################################################
-#Super Pi Power! - Jeremy Falling 2013	
-#Used to operate relays connected to specified pins. Does not use the bcm2835 perl library.
+#****************************************************************************
+#*   Super Pi Power!                                                        *
+#*   Turn outlets on and off from a webui running on the Pi!                *
+#*                                                                          *
+#*   Copyright (C) 2013 by Jeremy Falling except where noted.               *
+#*                                                                          *
+#*   This program is free software: you can redistribute it and/or modify   *
+#*   it under the terms of the GNU General Public License as published by   *
+#*   the Free Software Foundation, either version 3 of the License, or      *
+#*   (at your option) any later version.                                    *
+#*                                                                          *
+#*   This program is distributed in the hope that it will be useful,        *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+#*   GNU General Public License for more details.                           *
+#*                                                                          *
+#*   You should have received a copy of the GNU General Public License      *
+#*   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+#****************************************************************************
 
 #############################################################################
 ###Start user options
@@ -18,6 +34,7 @@ my $mode = 1;
 
 ### End user options  (**STOP EDITING!**)
 #############################################################################
+
 
 
 my $progamName = "Super Pi Power!";
